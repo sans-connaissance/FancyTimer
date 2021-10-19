a593f3dbdc156dfc88046533f2aa106dedfe43a4
sans-connaissance <davemalicke@gmail.com>
2021-10-19 10:09:13 -0400

    Initial Commit

---
+++ FancyTimer/FancyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FancyTimerApp.swift
+//  FancyTimer
+//
+//  Created by David Malicke on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FancyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
